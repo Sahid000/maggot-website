3d597076d6b268355ad3d5c4dfccc9f21a8e3782
din-islam-rajin <din.islam.rajin572@gmail.com>
2025-08-13 21:45:11 +0600

    issue fix #0.1.1

--- src/components/ui/DownloadAppSection/DownloadAppModal.tsx
+++ src/components/ui/DownloadAppSection/DownloadAppModal.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "antd";
-import Image from "next/image";
-import { allIcons } from "../../../../public/assets/AllImages";
 import ComingSoonModal from "@/components/ui/ComingSoonModal";
 
 const DownloadAppModal = () => {
@@ -23,14 +21,14 @@ const DownloadAppModal = () => {
           }}
           className=" text-start gap-1 py-8  md:w-[200px] bg-base-color border-none rounded-xl"
         >
-          <Image
+          {/* <Image
             src={allIcons.appleStore}
             alt="apple_store"
             width={0}
             height={0}
             sizes="100vw"
             className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px] md:h-[40px] md:w-[40px]"
-          />
+          /> */}
           <div>
             <p className="text-xs text-primary-color mb-1">Download on the</p>
             <h4 className="text-primary-color text-sm sm:text-lg md:text-xl">
@@ -44,14 +42,14 @@ const DownloadAppModal = () => {
           }}
           className=" text-start gap-1 py-8  md:w-[200px] bg-base-color border-none rounded-xl"
         >
-          <Image
+          {/* <Image
             src={allIcons.playstore}
             alt="play_store"
             width={0}
             height={0}
             sizes="100vw"
             className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px] md:h-[40px] md:w-[40px]"
-          />
+          /> */}
           <div>
             <p className="text-xs text-primary-color mb-1">Download on the</p>
             <h4 className="text-primary-color text-sm sm:text-lg md:text-xl">
